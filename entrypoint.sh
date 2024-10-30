ec1a9b1ff899c380a25cb3bd003a9ac301a46e41
Jignesh Nayi <jig_nayi@hotmail.com>
2024-10-30 14:23:09 +0000

    Fix Test Report as per CDP guideline

--- entrypoint.sh
+++ entrypoint.sh
@@ -12,14 +12,7 @@ JM_SCENARIOS=${JM_HOME}/scenarios
 JM_REPORTS=${JM_HOME}/reports
 JM_LOGS=${JM_HOME}/logs
 
-rm -f ${JM_REPORTS}/*.html ${JM_REPORTS}/*.json
-rm -rf ${JM_REPORTS}/content ${JM_REPORTS}/sbadmin2*
-
 mkdir -p ${JM_REPORTS} ${JM_LOGS}
-
-if [ -z "${TEST_SCENARIO}" ]; then
-  TEST_SCENARIO=test
-fi
 
 SCENARIOFILE=${JM_SCENARIOS}/${TEST_SCENARIO}.jmx
 REPORTFILE=${NOW}-perftest-${TEST_SCENARIO}-report.csv
@@ -30,13 +23,16 @@ jmeter -n -t ${SCENARIOFILE} -e -l "${REPORTFILE}" -o ${JM_REPORTS} -j ${LOGFILE
 test_exit_code=$?
 
 # Publish the results into S3 so they can be displayed in the CDP Portal
-# CDP Portal assumes test suite always produce a single html file
 if [ -n "$RESULTS_OUTPUT_S3_PATH" ]; then
+  # Copy the CSV report file and the generated report files to the S3 bucket
    if [ -f "$JM_REPORTS/index.html" ]; then
-      aws s3 cp "$JM_REPORTS/index.html" "$RESULTS_OUTPUT_S3_PATH/index.html"
-      echo "Test results published to $RESULTS_OUTPUT_S3_PATH"
+      aws --endpoint-url=$S3_ENDPOINT s3 cp "$REPORTFILE" "$RESULTS_OUTPUT_S3_PATH/$REPORTFILE"
+      aws --endpoint-url=$S3_ENDPOINT s3 cp "$JM_REPORTS" "$RESULTS_OUTPUT_S3_PATH" --recursive
+      if [ $? -eq 0 ]; then
+        echo "CSV report file and test results published to $RESULTS_OUTPUT_S3_PATH"
+      fi
    else
-      echo "$JM_REPORTS is not found"
+      echo "$JM_REPORTS/index.html is not found"
       exit 1
    fi
 else
