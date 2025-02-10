1ba411aa2a9f0eaef8f40f8c4f9ec17dc7f88c9f
Jignesh Nayi <jig_nayi@hotmail.com>
2025-02-10 16:37:33 +0000

    Bump up memory for jxm

--- entrypoint.sh
+++ entrypoint.sh
@@ -18,7 +18,7 @@ REPORTFILE=${NOW}-perftest-${TEST_SCENARIO}-report.csv
 LOGFILE=${JM_LOGS}/perftest-${TEST_SCENARIO}.log
 
 # Run the test suite
-jmeter -n -t ${SCENARIOFILE} -e -l "${REPORTFILE}" -o ${JM_REPORTS} -j ${LOGFILE} -f -Jenv="${ENVIRONMENT}"
+JVM_ARGS="-Xmx7g" jmeter -n -t ${SCENARIOFILE} -e -l "${REPORTFILE}" -o ${JM_REPORTS} -j ${LOGFILE} -f -Jenv="${ENVIRONMENT}" 
 test_exit_code=$?
 
 # Publish the results into S3 so they can be displayed in the CDP Portal
